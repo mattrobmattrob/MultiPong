a780b46322f30643610a4728ce0065d773f729c7
Matt Robinson <mrobinson@strava.com>
2015-12-23 03:40:27 -0600

    Fix team identity

--- MultiPong iPhone/GameViewController.m
+++ MultiPong iPhone/GameViewController.m
@@ -30,58 +30,58 @@
         }
     }];
 
-    // create a new scene
-    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
-
-    // create and add a camera to the scene
-    SCNNode *cameraNode = [SCNNode node];
-    cameraNode.camera = [SCNCamera camera];
-    [scene.rootNode addChildNode:cameraNode];
-    
-    // place the camera
-    cameraNode.position = SCNVector3Make(0, 0, 15);
-    
-    // create and add a light to the scene
-    SCNNode *lightNode = [SCNNode node];
-    lightNode.light = [SCNLight light];
-    lightNode.light.type = SCNLightTypeOmni;
-    lightNode.position = SCNVector3Make(0, 10, 10);
-    [scene.rootNode addChildNode:lightNode];
-    
-    // create and add an ambient light to the scene
-    SCNNode *ambientLightNode = [SCNNode node];
-    ambientLightNode.light = [SCNLight light];
-    ambientLightNode.light.type = SCNLightTypeAmbient;
-    ambientLightNode.light.color = [UIColor darkGrayColor];
-    [scene.rootNode addChildNode:ambientLightNode];
-    
-    // retrieve the ship node
-    SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:YES];
-    
-    // animate the 3d object
-    [ship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
-    
-    // retrieve the SCNView
-    SCNView *scnView = (SCNView *)self.view;
-    
-    // set the scene to the view
-    scnView.scene = scene;
-    
-    // allows the user to manipulate the camera
-    scnView.allowsCameraControl = YES;
-        
-    // show statistics such as fps and timing information
-    scnView.showsStatistics = YES;
-
-    // configure the view
-    scnView.backgroundColor = [UIColor blackColor];
-    
-    // add a tap gesture recognizer
-    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
-    NSMutableArray *gestureRecognizers = [NSMutableArray array];
-    [gestureRecognizers addObject:tapGesture];
-    [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
-    scnView.gestureRecognizers = gestureRecognizers;
+//    // create a new scene
+//    SCNScene *scene = [SCNScene sceneNamed:@"art.scnassets/ship.scn"];
+//
+//    // create and add a camera to the scene
+//    SCNNode *cameraNode = [SCNNode node];
+//    cameraNode.camera = [SCNCamera camera];
+//    [scene.rootNode addChildNode:cameraNode];
+//    
+//    // place the camera
+//    cameraNode.position = SCNVector3Make(0, 0, 15);
+//    
+//    // create and add a light to the scene
+//    SCNNode *lightNode = [SCNNode node];
+//    lightNode.light = [SCNLight light];
+//    lightNode.light.type = SCNLightTypeOmni;
+//    lightNode.position = SCNVector3Make(0, 10, 10);
+//    [scene.rootNode addChildNode:lightNode];
+//    
+//    // create and add an ambient light to the scene
+//    SCNNode *ambientLightNode = [SCNNode node];
+//    ambientLightNode.light = [SCNLight light];
+//    ambientLightNode.light.type = SCNLightTypeAmbient;
+//    ambientLightNode.light.color = [UIColor darkGrayColor];
+//    [scene.rootNode addChildNode:ambientLightNode];
+//    
+//    // retrieve the ship node
+//    SCNNode *ship = [scene.rootNode childNodeWithName:@"ship" recursively:YES];
+//    
+//    // animate the 3d object
+//    [ship runAction:[SCNAction repeatActionForever:[SCNAction rotateByX:0 y:2 z:0 duration:1]]];
+//    
+//    // retrieve the SCNView
+//    SCNView *scnView = (SCNView *)self.view;
+//    
+//    // set the scene to the view
+//    scnView.scene = scene;
+//    
+//    // allows the user to manipulate the camera
+//    scnView.allowsCameraControl = YES;
+//        
+//    // show statistics such as fps and timing information
+//    scnView.showsStatistics = YES;
+//
+//    // configure the view
+//    scnView.backgroundColor = [UIColor blackColor];
+//    
+//    // add a tap gesture recognizer
+//    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+//    NSMutableArray *gestureRecognizers = [NSMutableArray array];
+//    [gestureRecognizers addObject:tapGesture];
+//    [gestureRecognizers addObjectsFromArray:scnView.gestureRecognizers];
+//    scnView.gestureRecognizers = gestureRecognizers;
 }
 
 - (void)viewDidAppear:(BOOL)animated
@@ -174,12 +174,12 @@
 
 #pragma mark - GKMatchmakerViewControllerDelegate
 
-#pragma mark - GKMatchmakerViewControllerDelegate
-
 // The user has cancelled matchmaking
 - (void)matchmakerViewControllerWasCancelled:(GKMatchmakerViewController *)viewController
 {
     NSLog(@"matchmakerViewControllerWasCancelled:");
+
+    [viewController dismissViewControllerAnimated:YES completion:nil];
 }
 
 // Matchmaking has failed with an error
